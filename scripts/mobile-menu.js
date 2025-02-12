0f15e93a2a7cbc1e18db41cadb5ed2fbba0c5dc0
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-02-13 00:15:17 +0500

    hero section

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // открытие мобильного меню
     const mobileMenu = document.getElementById('menu-mobile-box');
     const openBtnMobileMenu = document.getElementById('open-mobile-menu');
     const closeBtnMenu = document.getElementById('close-mobile-menu');
@@ -17,4 +19,26 @@ document.addEventListener('DOMContentLoaded', () => {
         openBtnMobileMenu.addEventListener('click', openMenu);
         closeBtnMenu.addEventListener('click', closeMenu);
     }
+
+// изменение голубого подсвечивания
+    const circleBlue = document.querySelector('.circle-blue');
+    const circleLightBlue = document.querySelector('.circle-lightblue');
+    const mouseOwnerNumber = document.querySelector('.hero__body-right');
+
+    if (circleBlue && circleLightBlue && mouseOwnerNumber) {
+
+        const mouseoverFank = () => {
+            circleBlue.classList.remove('none');
+            circleLightBlue.classList.add('none');
+        }
+
+        const mouseoutFank = () => {
+            circleBlue.classList.add('none');
+            circleLightBlue.classList.remove('none');
+        }
+
+        mouseOwnerNumber.addEventListener('mouseover', mouseoverFank);
+        mouseOwnerNumber.addEventListener('mouseout', mouseoutFank);
+    }
+
 })
