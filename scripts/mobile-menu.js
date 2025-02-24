e2e6efb3c513d614d25ac268ab4a8b11cb8ac2c8
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-02-24 12:20:27 +0500

    222

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -41,92 +41,92 @@ document.addEventListener('DOMContentLoaded', () => {
         mouseOwnerNumber.addEventListener('mouseout', mouseoutFank);
     }
 
-    // Функция для проверки позиции элемента
-    function checkElementPositionOneBox() {
-        const section = document.querySelector('.redactor-coda');
-        const sectionStickyOne = document.querySelector('.jump-page-one');
-
-        if (!section && !sectionStickyOne) return; // Проверка существования элемента
-
-        const rect = section.getBoundingClientRect();
-        const isAtTop = rect.top <= 0;
-
-        if (isAtTop) {
-            console.log('Элемент достиг верха окна!');
-            // Здесь можно добавить свои действия
-            sectionStickyOne.style.position = `relative`;
-        } else {
-            sectionStickyOne.style.position = `sticky`;
-        }
-    }
-
-    function checkElementPositionTwoBox() {
-        const section = document.querySelector('.cards');
-        let cardsHeight = section.offsetHeight;
-        // console.log(cardsHeight);
-        const sectionElems = document.querySelectorAll('.card-item');
-        // console.log(sectionElems);
-        const sectionStickyTwo = document.querySelector('.jump-page-two');
-
-        if (!section && !sectionStickyTwo) return; // Проверка существования элемента
-
-        const rect = section.getBoundingClientRect();
-        const isAtTop = rect.top <= 0;
-
-        if (isAtTop) {
-            console.log('Элемент достиг верха окна!');
-            // Здесь можно добавить свои действия
-            sectionStickyTwo.style.position = `relative`;
-
-            sectionElems.forEach((elem) => {
-                elem.style.position = `sticky`;
-            })
-            // sectionElem.style.position = `sticky`;
-        } else {
-            sectionStickyTwo.style.position = `sticky`;
-            sectionElems.forEach((elem) => {
-                elem.style.position = `relative`;
-            })
-
-        }
-    }
-
-// Запуск проверки при скролле и загрузке страницы
-    window.addEventListener('scroll', function () {
-        requestAnimationFrame(checkElementPositionOneBox);
-        requestAnimationFrame(checkElementPositionTwoBox);
-    });
-
-// Первоначальная проверка при загрузке
-    window.addEventListener('DOMContentLoaded', checkElementPositionOneBox, checkElementPositionTwoBox);
-
-    // табы
-    // Получаем все элементы табов
-    const tabButtons = document.querySelectorAll('.tab-btn');
-    const tabContents = document.querySelectorAll('.tab-content');
-
-    if (tabButtons && tabContents) {
-
-        // Активируем первый таб при загрузке
-        tabButtons[0].classList.add('tab-btn-active');
-        tabContents[0].classList.add('tab-content-active');
-        // Добавляем обработчик событий для каждой кнопки
-        tabButtons.forEach(button => {
-
-            button.addEventListener('click', () => {
-                // Удаляем активный класс у всех кнопок и контента
-                tabButtons.forEach(btn => btn.classList.remove('tab-btn-active'));
-                tabContents.forEach(content => content.classList.remove('tab-content-active'));
-
-                // Добавляем активный класс к выбранной кнопке
-                button.classList.add('tab-btn-active');
-
-                // Показываем соответствующий контент
-                const targetTab = document.getElementById(button.dataset.tab);
-                targetTab.classList.add('tab-content-active');
-            });
-        });
-    }
-
+//     // Функция для проверки позиции элемента
+//     function checkElementPositionOneBox() {
+//         const section = document.querySelector('.redactor-coda');
+//         const sectionStickyOne = document.querySelector('.jump-page-one');
+//
+//         if (!section && !sectionStickyOne) return; // Проверка существования элемента
+//
+//         const rect = section.getBoundingClientRect();
+//         const isAtTop = rect.top <= 0;
+//
+//         if (isAtTop) {
+//             console.log('Элемент достиг верха окна!');
+//             // Здесь можно добавить свои действия
+//             sectionStickyOne.style.position = `relative`;
+//         } else {
+//             sectionStickyOne.style.position = `sticky`;
+//         }
+//     }
+//
+//     function checkElementPositionTwoBox() {
+//         const section = document.querySelector('.cards');
+//         let cardsHeight = section.offsetHeight;
+//         // console.log(cardsHeight);
+//         const sectionElems = document.querySelectorAll('.card-item');
+//         // console.log(sectionElems);
+//         const sectionStickyTwo = document.querySelector('.jump-page-two');
+//
+//         if (!section && !sectionStickyTwo) return; // Проверка существования элемента
+//
+//         const rect = section.getBoundingClientRect();
+//         const isAtTop = rect.top <= 0;
+//
+//         if (isAtTop) {
+//             console.log('Элемент достиг верха окна!');
+//             // Здесь можно добавить свои действия
+//             sectionStickyTwo.style.position = `relative`;
+//
+//             sectionElems.forEach((elem) => {
+//                 elem.style.position = `sticky`;
+//             })
+//             // sectionElem.style.position = `sticky`;
+//         } else {
+//             sectionStickyTwo.style.position = `sticky`;
+//             sectionElems.forEach((elem) => {
+//                 elem.style.position = `relative`;
+//             })
+//
+//         }
+//     }
+//
+// // Запуск проверки при скролле и загрузке страницы
+//     window.addEventListener('scroll', function () {
+//         requestAnimationFrame(checkElementPositionOneBox);
+//         requestAnimationFrame(checkElementPositionTwoBox);
+//     });
+//
+// // Первоначальная проверка при загрузке
+//     window.addEventListener('DOMContentLoaded', checkElementPositionOneBox, checkElementPositionTwoBox);
+//
+//     // табы
+//     // Получаем все элементы табов
+//     const tabButtons = document.querySelectorAll('.tab-btn');
+//     const tabContents = document.querySelectorAll('.tab-content');
+//
+//     if (tabButtons && tabContents) {
+//
+//         // Активируем первый таб при загрузке
+//         tabButtons[0].classList.add('tab-btn-active');
+//         tabContents[0].classList.add('tab-content-active');
+//         // Добавляем обработчик событий для каждой кнопки
+//         tabButtons.forEach(button => {
+//
+//             button.addEventListener('click', () => {
+//                 // Удаляем активный класс у всех кнопок и контента
+//                 tabButtons.forEach(btn => btn.classList.remove('tab-btn-active'));
+//                 tabContents.forEach(content => content.classList.remove('tab-content-active'));
+//
+//                 // Добавляем активный класс к выбранной кнопке
+//                 button.classList.add('tab-btn-active');
+//
+//                 // Показываем соответствующий контент
+//                 const targetTab = document.getElementById(button.dataset.tab);
+//                 targetTab.classList.add('tab-content-active');
+//             });
+//         });
+//     }
+//
 
 })
