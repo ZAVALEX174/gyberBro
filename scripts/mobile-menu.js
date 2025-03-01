ac9727e331c07c0f430c1965d949fc6d5fb926a6
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-03-01 10:48:15 +0500

    fixed logic

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -4,20 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.getElementById('menu-mobile-box');
     const openBtnMobileMenu = document.getElementById('open-mobile-menu');
     const closeBtnMenu = document.getElementById('close-mobile-menu');
+    const menuLinks = document.querySelectorAll('.menu-mobile-box__link');
 
     if (mobileMenu && openBtnMobileMenu && closeBtnMenu) {
         const openMenu = () => {
             mobileMenu.classList.add('menu-mobile-box-visible');
             closeBtnMenu.classList.add('header__menu-mobile-btn_close-visible');
+            document.body.style.overflow = 'hidden';
         }
 
         const closeMenu = () => {
             mobileMenu.classList.remove('menu-mobile-box-visible');
             closeBtnMenu.classList.remove('header__menu-mobile-btn_close-visible');
+            document.body.style.overflow = '';
         }
 
         openBtnMobileMenu.addEventListener('click', openMenu);
         closeBtnMenu.addEventListener('click', closeMenu);
+        menuLinks.forEach(link => {
+            link.addEventListener('click', closeMenu);
+        })
     }
 
 // изменение голубого подсвечивания
