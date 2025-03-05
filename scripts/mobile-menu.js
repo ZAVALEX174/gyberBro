d20b47b8d122b30df632cf89f28a1a80882af014
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-03-05 10:43:18 +0500

    fixed  video redactor code

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,3 +1,91 @@
+function updateVideoSources() {
+    const isMobile = window.innerWidth <= 768;
+
+    // Получаем ВСЕ source элементы внутри видео
+    const sources = document.querySelectorAll('#first-video source');
+    console.log(sources)
+
+    sources.forEach(source => {
+        const src = isMobile ?
+            source.dataset.srcMobile :
+            source.dataset.srcDesktop;
+
+        if (source.src !== src) {
+            // Меняем src у source элемента
+            source.src = src;
+            // Получаем родительский video элемент и перезагружаем его
+            const video = source.parentElement;
+            video.load();
+        }
+    });
+}
+
+// Инициализация
+window.addEventListener('load', updateVideoSources);
+window.addEventListener('resize', updateVideoSources);
+
+// запуск видео
+// document.addEventListener('DOMContentLoaded', () => {
+
+
+    const firstVideo = document.getElementById('first-video');
+    const secondVideo = document.getElementById('second-video');
+    const hackButton = document.getElementById('hack-button');
+    const landingContent = document.getElementById('landing-content');
+    const videoContent = document.getElementById('video-container');
+
+    // // Проверяем, была ли анимация уже воспроизведена
+    // if (localStorage.getItem('animationPlayed')) {
+    //     // Если анимация уже воспроизведена, сразу показываем лендинг
+    //     firstVideo.style.display = 'none';
+    //     secondVideo.style.display = 'none';
+    //     videoContent.style.display = 'none';
+    //     landingContent.style.display = 'block';
+    //     landingContent.classList.add('show'); // Плавное появление
+    //     return; // Прерываем выполнение
+    // }
+
+    firstVideo.addEventListener('play', () => {
+        setTimeout(() => {
+
+            setTimeout(() => {
+                hackButton.classList.add('hack-button-visible'); // Плавное появление
+            }, 50);
+        }, 5000);
+    });
+
+    // Обработчик нажатия на кнопку
+    hackButton.addEventListener('click', () => {
+        // Скрываем кнопку
+        hackButton.style.display = 'none';
+        firstVideo.style.display = 'none';
+        // Запускаем второе видео
+        secondVideo.play();
+        secondVideo.style.opacity = '1'; // Показываем второе видео
+
+        // Плавное изменение прозрачности второго видео
+        setTimeout(() => {
+            secondVideo.style.opacity = '0'; // Плавно скрываем второе видео
+        }, 5000); // 6 секунд
+
+        // После завершения второго видео и задержки показываем лендинг
+        setTimeout(() => {
+            // Скрываем видео
+
+            secondVideo.style.display = 'none';
+            videoContent.style.display = 'none';
+
+            // Показываем контент лендинга
+            landingContent.style.display = 'block';
+            landingContent.classList.add('show'); // Плавное появление
+
+            // // Сохраняем состояние в localStorage
+            // localStorage.setItem('animationPlayed', 'true');
+        }, 5500); // 6 секунд + 3 секунды задержки
+    });
+// });
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // открытие мобильного меню
@@ -125,90 +213,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
-function updateVideoSources() {
-    const isMobile = window.innerWidth <= 768;
-
-    // Получаем ВСЕ source элементы внутри видео
-    const sources = document.querySelectorAll('#first-video source');
-    console.log(sources)
-
-    sources.forEach(source => {
-        const src = isMobile ?
-            source.dataset.srcMobile :
-            source.dataset.srcDesktop;
-
-        if (source.src !== src) {
-            // Меняем src у source элемента
-            source.src = src;
-            // Получаем родительский video элемент и перезагружаем его
-            const video = source.parentElement;
-            video.load();
-        }
-    });
-}
-
-// Инициализация
-window.addEventListener('load', updateVideoSources);
-window.addEventListener('resize', updateVideoSources);
-
-// запуск видео
-document.addEventListener('DOMContentLoaded', () => {
-
-
-    const firstVideo = document.getElementById('first-video');
-    const secondVideo = document.getElementById('second-video');
-    const hackButton = document.getElementById('hack-button');
-    const landingContent = document.getElementById('landing-content');
-    const videoContent = document.getElementById('video-container');
-
-    // // Проверяем, была ли анимация уже воспроизведена
-    // if (localStorage.getItem('animationPlayed')) {
-    //     // Если анимация уже воспроизведена, сразу показываем лендинг
-    //     firstVideo.style.display = 'none';
-    //     secondVideo.style.display = 'none';
-    //     videoContent.style.display = 'none';
-    //     landingContent.style.display = 'block';
-    //     landingContent.classList.add('show'); // Плавное появление
-    //     return; // Прерываем выполнение
-    // }
-
-    firstVideo.addEventListener('play', () => {
-        setTimeout(() => {
-
-            setTimeout(() => {
-                hackButton.classList.add('hack-button-visible'); // Плавное появление
-            }, 50);
-        }, 5000);
-    });
-
-    // Обработчик нажатия на кнопку
-    hackButton.addEventListener('click', () => {
-        // Скрываем кнопку
-        hackButton.style.display = 'none';
-        firstVideo.style.display = 'none';
-        // Запускаем второе видео
-        secondVideo.play();
-        secondVideo.style.opacity = '1'; // Показываем второе видео
-
-        // Плавное изменение прозрачности второго видео
-        setTimeout(() => {
-            secondVideo.style.opacity = '0'; // Плавно скрываем второе видео
-        }, 6000); // 6 секунд
-
-        // После завершения второго видео и задержки показываем лендинг
-        setTimeout(() => {
-            // Скрываем видео
-
-            secondVideo.style.display = 'none';
-            videoContent.style.display = 'none';
-
-            // Показываем контент лендинга
-            landingContent.style.display = 'block';
-            landingContent.classList.add('show'); // Плавное появление
-
-            // // Сохраняем состояние в localStorage
-            // localStorage.setItem('animationPlayed', 'true');
-        }, 9000); // 6 секунд + 3 секунды задержки
-    });
-});
